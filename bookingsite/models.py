e1897653f3bc83e77bbb7703b6c2e98e973620d2
Rhys Yearwood <ryearwood1@live.com>
2024-07-08 12:35:49 +0000

    made migrations of updated user model, new booking and resource model. add temp httpsresponse views. add urls path in bookingsite app

--- bookingsite/models.py
+++ bookingsite/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.auth.models import AbstractUser
 from cloudinary.models import CloudinaryField
 
 
-class User(AbstractUser):
+class User(User):
     phone_number = models.CharField(max_length=15)
 
 
@@ -25,24 +24,3 @@ class Booking(models.Model):
         ('cancelled', 'Cancelled')
     ])
     payment_details = models.TextField()
-
-
-# class Payment(models.Model):
-#     booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
-#     amount = models.DecimalField(max_digits=10, decimal_places=2)
-#     payment_date = models.DateTimeField()
-#     payment_method = models.CharField(max_length=50)
-
-
-# class Comment(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
-#     rating = models.IntegerField()
-#     comments = models.TextField()
-
-
-# class Schedule(models.Model):
-#     resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
-#     start_time = models.TimeField()
-#     end_time = models.TimeField()
-#     date = models.DateField()
