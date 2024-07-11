361804b95f50fcc6c880ec7eee0281c5bcd51212
Rhys Yearwood <ryearwood1@live.com>
2024-07-11 09:39:48 +0000

    add views,  htmls,url pathing for bookings. pre migration, fingers crossed

--- bookingsite/urls.py
+++ bookingsite/urls.py
@@ -5,5 +5,11 @@ from . import views
 urlpatterns = [
     path('nav/', views.NavigationView.as_view(), name='nav'),
     path('', views.EntryView.as_view(), name = 'home'),
-    path('booking/', views.BookingView.as_view(), name = 'booking')
+    path('booking/', views.BookingView.as_view(), name = 'booking'),
+    path('create/', views.create_booking, name='create_booking'),
+    path('edit/<int:booking_id>/', views.edit_booking, name='edit_booking'),
+    path('delete/<int:booking_id>/', views.delete_booking, name='delete_booking'),
+    path('<int:booking_id>/', views.show_booking, name='show_booking'),
+    path('show_all/', views.show_all_booking, name='show_all_booking'),
+    path('booking-success/<int:booking_id>/', views.booking_success, name='booking-success'),
 ]
--- bookingsite/views.py
+++ bookingsite/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.views import LoginView
 from . import views
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .models import Booking, Resource
+
 
 class NavigationView(generic.ListView):
     template_name = "bookingsite/index.html"
@@ -22,3 +24,51 @@ class BookingView(generic.ListView):
 
     def get_queryset(self):
         return []
+
+@login_required
+def create_commission(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.client = request.user.profile
+            booking.save()
+            return redirect('show_booking', booking_id=booking.id)
+    else:
+        form = BookingForm()
+    return render(request, 'bookingsite/booking.html', {'form': form})
+
+@login_required
+def edit_booking(request, booking_id):
+    booking = BookingForm.objects.get(pk=booking_id)
+    if request.method == 'POST':
+        form = BookingForm(request.POST, instance=booking)
+        if form.is_valid():
+            form.save()
+            return redirect('show_booking', booking_id=booking.id)
+    else:
+        form = BookingForm(instance=booking)
+    return render(request, 'bookingsite/edit_booking.html', {'form': form, 'booking': booking})
+
+@login_required
+def delete_booking(request, booking_id):
+    booking = Booking.objects.get(pk=booking_id)
+    if request.method == 'POST':
+        booking.delete()
+        return redirect('show_all_booking')
+    return render(request, 'bookingsite/delete_booking.html', {'booking': booking})
+
+@login_required
+def show_booking(request, booking_id):
+    booking = Booking.objects.get(pk=booking_id)
+    return render(request, 'bookingsite/show_booking.html', {'booking': booking})
+
+@login_required
+def show_all_booking(request):
+    bookings = Booking.objects.filter(client=request.user.profile)
+    return render(request, 'bookingsite/show_all_booking.html', {'bookings': bookings})
+
+@login_required
+def booking_success(request, booking_id):
+    booking = get_object_or_404(Booking, pk=booking_id)
+    return render(request, 'home/booking_success.html', {'booking': booking})
