927087870f5faa1f5629880b6df92efaf7bf96e9
Rhys Yearwood <ryearwood1@live.com>
2024-07-12 08:48:12 +0000

    add _str_ def to make resources return as names instead of resource object

--- bookingsite/models.py
+++ bookingsite/models.py
@@ -13,6 +13,9 @@ class Resource(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
